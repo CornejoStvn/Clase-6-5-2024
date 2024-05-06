2c057060e27dd4e661770a3ce955a8d28a8dab52
CornejoStvn <164422216+CornejoStvn@users.noreply.github.com>
2024-05-06 12:42:40 -0600

    Add files via upload

---
+++ Prueba de while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i=1 ;
+    while (i<11)
+{
+    cout << 12*1<<"\n";
+    i++;
+}
+return 0;
+}
---
+++ while2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int i=1, res = 0; 
+while (i<=12)
+{
+    res=i*12;
+    cout<<"12 X" << i <<" = "<<res<<"\n";
+ i++;
+}
+return 0;
+}
